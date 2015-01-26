07cc9e257772faca9fffa84a59bf4baa7c6dbf2e
dsapikas <dsapikas@gmail.com>
2015-01-26 21:49:36 +0200

    Data Class Update

--- src/s2elab/Data.java
+++ src/s2elab/Data.java
@@ -7,4 +7,5 @@ public class Data {
 	static String passwd = "";
 	static String classpath = "com.mysql.jdbc.Driver";
 	static String queryCountCheckin = "SELECT COUNT(*) as count FROM checkins";
+	static String queryCountPackages = "SELECT COUNT(*) as count FROM packages";
 }
