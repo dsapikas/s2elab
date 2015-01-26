77d88a4ad52213a864978534a5d774982d1d9e6e
dsapikas <dsapikas@gmail.com>
2015-01-26 21:49:44 +0200

    Package Checkin Update

--- src/s2elab/checkin.java
+++ src/s2elab/checkin.java
@@ -11,20 +11,23 @@ public class Checkin {
 	int RID;
 	String place;
 	String datetime;
+	String status;
+	public String[] listdata;
 	
 	Checkin()
 	{
 		
 	}
 	
-	private Checkin(int _rid, int _pid, String _place, String _datetime)
+	private Checkin(int _rid, int _pid, String _place, String _datetime,String _Status)
 	{
 		PID = _pid;
 		RID = _rid;
 		place = _place;
 		datetime = _datetime;
-	
-		//System.out.println("PID = " + PID + " RID = " + RID + " place = " + place + " datetime = " + datetime);
+		status = _Status;
+		listdata = new String[] {datetime,place,status};
+		//System.out.println("PID = " + PID + " RID = " + RID + " place = " + place + " status = " + status);
 	}
 	
 	public static Checkin[] getCheckins()
@@ -44,7 +47,7 @@ public class Checkin {
 			
 			while(rs.next()){
 				//System.out.println(rs.getString("address"));
-				Checkin c = new Checkin(rs.getInt("PID"),rs.getInt("RID"),rs.getString("place"),rs.getString("Date"));
+				Checkin c = new Checkin(rs.getInt("PID"),rs.getInt("RID"),rs.getString("place"),rs.getString("Date"),rs.getString("status"));
 				ch[rs.getRow()-1] = c;
 			}
 		} catch (Exception e) {
@@ -55,14 +58,42 @@ public class Checkin {
 		return ch;
 	}
 	
-	public static void add(String place, int PID)
+	public static Checkin[] getCheckins(int PID)
+	{
+		java.sql.Connection conn = null;
+		Checkin ch[];
+		try {
+			
+			Class.forName(Data.classpath);
+			conn = DriverManager.getConnection(Data.db,Data.uname,Data.passwd);
+			ResultSet rsa = conn.createStatement().executeQuery(Data.queryCountCheckin + " where PID = " + PID);
+			rsa.next();
+			
+			ch = new Checkin[rsa.getInt("count")];
+			
+			ResultSet rs = conn.createStatement().executeQuery("Select * from  Checkins where PID = " + PID);
+			
+			while(rs.next()){
+				//System.out.println(rs.getString("address"));
+				Checkin c = new Checkin(rs.getInt("PID"),rs.getInt("RID"),rs.getString("place"),rs.getString("Date"),rs.getString("status"));
+				ch[rs.getRow()-1] = c;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.err.println("gamithike to syban");
+			ch = new Checkin[1];
+		}
+		return ch;
+	}
+	
+	public static void add(String place, int PID, String Status)
 	{
 		java.sql.Connection conn = null;
 		try {
 			Class.forName(Data.classpath);
 			conn = DriverManager.getConnection(Data.db,Data.uname,Data.passwd);
-			String query = "Insert into Checkins values (NULL,'" + place + "'," + PID + ",NULL)";
-			System.out.println(query);
+			String query = "Insert into Checkins values (NULL,'" + place + "'," + PID + ",NULL,'" + Status +"')";
+			//System.out.println(query);
 			conn.createStatement().executeUpdate(query);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
