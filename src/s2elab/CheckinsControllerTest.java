b3a940b1e19778c32d44b9a9cb592e4e873fa9cc
dsapikas <dsapikas@gmail.com>
2015-01-28 16:43:23 +0200

    Submit final

---
+++ src/s2elab/CheckinsControllerTest.java
@@ -0,0 +1,14 @@
+package s2elab;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CheckinsControllerTest extends CheckinsController {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
